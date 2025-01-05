a4da54a685a3b8c060fdd9a27e04fe62f03f28c4
VeryRealBreadik <xjie6yiiiek228822@gmail.com>
2025-01-05 15:53:55 +0300

    Added FIXME to some methods that do not work after previous changes

--- app/database/crud/pin_collections_crud.py
+++ app/database/crud/pin_collections_crud.py
@@ -34,31 +34,31 @@ class CRUDPinCollection:
         await db.refresh(db_pin_collection)
         return db_pin_collection
     
-    async def add_pin_to_collection(self, db: AsyncSession, pin_id: int, collection_id: int):
-        pin_result = await db.execute(select(Pin).where(Pin.id == pin_id))
-        db_pin = pin_result.scalars().first()
-        collection_result = await db.execute(select(PinCollection).where(PinCollection.id == collection_id))
-        db_collection = collection_result.scalars().first()
-        if db_pin is None and db_collection is None:
-            return None
-        db_association = PinCollectionAssociation(pin_id=db_pin.id, collection_id=db_collection.id)
-        db.add(db_association)
-        await db.commit()
-        await db.refresh(db_association)
-        return db_association
+    # async def add_pin_to_collection(self, db: AsyncSession, pin_id: int, collection_id: int): # FIXME: this method is not working anymore
+    #     pin_result = await db.execute(select(Pin).where(Pin.id == pin_id))
+    #     db_pin = pin_result.scalars().first()
+    #     collection_result = await db.execute(select(PinCollection).where(PinCollection.id == collection_id))
+    #     db_collection = collection_result.scalars().first()
+    #     if db_pin is None and db_collection is None:
+    #         return None
+    #     db_association = PinCollectionAssociation(pin_id=db_pin.id, collection_id=db_collection.id)
+    #     db.add(db_association)
+    #     await db.commit()
+    #     await db.refresh(db_association)
+    #     return db_association
     
     # DELETE
     async def delete_pin_collection(self, db: AsyncSession, db_pin_collection: PinCollection):
         await db.delete(db_pin_collection)
         await db.commit()
     
-    async def remove_pin_from_collection(self, db: AsyncSession, pin_id: int, collection_id: int):
-        result = await db.execute(select(PinCollectionAssociation).where(PinCollectionAssociation.pin_id == pin_id, PinCollectionAssociation.collection_id == collection_id))
-        db_association = result.scalars().first()
-        if db_association is None:
-            return None
-        await db.delete(db_association)
-        await db.commit()
-        return db_association
+    # async def remove_pin_from_collection(self, db: AsyncSession, pin_id: int, collection_id: int): # FIXME: this method is not working anymore
+    #     result = await db.execute(select(PinCollectionAssociation).where(PinCollectionAssociation.pin_id == pin_id, PinCollectionAssociation.collection_id == collection_id))
+    #     db_association = result.scalars().first()
+    #     if db_association is None:
+    #         return None
+    #     await db.delete(db_association)
+    #     await db.commit()
+    #     return db_association
 
 crud_collection = CRUDPinCollection()
